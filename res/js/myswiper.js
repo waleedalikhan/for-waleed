8be61608485a786462caaf9ed0271ce96646a8b4
waleed <waleed@Waleed.local>
2020-02-18 15:33:08 +0500

    new pages

--- res/js/myswiper.js
+++ res/js/myswiper.js
@@ -2,7 +2,7 @@ var lastCat = "cat1";
 var mySwiper = new Swiper(".swiper-container", {
   // slidesPerView: 'auto',
   slidesPerView: 6,
-  spaceBetween: 30,
+  spaceBetween: 20,
   freeMode: !0,
   loop: !1,
   watchOverflow: !0,
@@ -44,3 +44,48 @@ var waypoints = $(".group-item ").waypoint({
     console.log("Last Cat2 : " + index);
   }
 });
+
+// ? Show/Less grid checkboxes
+function show() {
+  var moreChecks = document.getElementById("more-checks");
+  var btn = document.getElementById("more-btn");
+  var btn2 = document.getElementById("less-btn")
+
+  moreChecks.style.display = "block";
+  btn.style.display = "none"
+  btn2.style.display = "block"
+}
+function less() {
+  var moreChecks = document.getElementById("more-checks");
+  var btn = document.getElementById("more-btn");
+  var btn2 = document.getElementById("less-btn")
+
+  moreChecks.style.display = "none";
+  btn.style.display = "block"
+  btn2.style.display = "none"
+}
+
+// ! Showing/Hiding Search Bar
+function showSearchBar() {
+  var searchBtn = document.getElementById("search-btn");
+  var dishList = document.getElementById("dish-list");
+  var searchBox = document.getElementById("search-box") ;
+  var angleLeft = document.getElementById("swiper-next")
+
+  searchBtn.style.display = "none";
+  dishList.style.display = "none";
+  angleLeft.style.display = "none";
+  searchBox.style.display = "flex";
+}
+
+function hideSearchBar() {
+  var searchBtn = document.getElementById("search-btn");
+  var dishList = document.getElementById("dish-list");
+  var searchBox = document.getElementById("search-box") ;
+  var angleLeft = document.getElementById("swiper-next")
+
+  searchBtn.style.display = "flex";
+  dishList.style.display = "flex";
+  angleLeft.style.display = "flex";
+  searchBox.style.display = "none";
+}
